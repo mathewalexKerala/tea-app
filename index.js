2479c8c2e34a08c582a45217bcab9a49e24dbf7d
Mathew Alex <mathewalexmasai@gmail.com>
2024-12-10 05:19:28 +0530

    add logger and winston

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 import 'dotenv/config'
+import logger from "./logger.js";
+import morgan from "morgan";
+
+
 
 import express from "express";
+
+
+
+const morganFormat = ":method :url :status :response-time ms";
+
+
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 app.use(express.json());
 
