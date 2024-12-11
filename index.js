131b0b747f3ffe5bd0547058f9d69e5b883e6ada
Mathew Alex <mathewalexmasai@gmail.com>
2024-12-11 19:53:22 +0530

    change the structure of index.js

--- index.js
+++ index.js
@@ -1,93 +1,63 @@
-import 'dotenv/config'
-import logger from "./logger.js";
-import morgan from "morgan";
-
-
-
 import express from "express";
 
+const app = express(); 
 
-
-const morganFormat = ":method :url :status :response-time ms";
-
-
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(
-  morgan(morganFormat, {
-    stream: {
-      write: (message) => {
-        const logObject = {
-          method: message.split(" ")[0],
-          url: message.split(" ")[1],
-          status: message.split(" ")[2],
-          responseTime: message.split(" ")[3],
-        };
-        logger.info(JSON.stringify(logObject));
-      },
-    },
-  })
-);
+const PORT =process.env.PORT || 3000;
 
 app.use(express.json());
 
 let teaData = [];
 
 let nextId = 1;
-
+//add a tea
 app.post("/teas", (req, res) => {
-  const { name, price } = req.body;
-
+  const { tea, price } = req.body;
   const newTea = {
-    id: nextId,
-    name,
+    id: nextId++,
+    tea,
     price,
   };
   teaData.push(newTea);
-  nextId++;
-  res.status(201).send(newTea);
+   res.status(201).send(newTea);
 });
 
+//get all tea
 app.get("/teas", (req, res) => {
-  res.status(200).send(teaData);
+   res.status(200).send(teaData);
 });
 
+//get single tea
 app.get("/teas/:id", (req, res) => {
-  res
-    .status(200)
-    .send(teaData.find((tea) => tea.id == parseInt(req.params.id)));
+  const Id = req.params.id - 1;
+ 
+   Id < teaData.length && Id >= 0
+    ? res.status(200).send(teaData[Id])
+    : res.status(404).send("not found");
 });
 
-//update tea
+//update an existing tea
 
 app.put("/teas/:id", (req, res) => {
-  const tea = teaData.find((tea) => tea.id == parseInt(req.params.id));
-  if (!tea) {
-    return res.status(404).send("Tea not found");
-  }
-  const { name, price } = req.body;
-  tea.name = name;
-  tea.price = price;
+ 
+ 
+  const index = parseInt(req.params.id) - 1;
+//logic for not entering body or params id
+  ( !req.body || Object.keys(req.body).length ===0)?res.status(400).send(' Input value  is missing'):(index < 0 || index >=teaData.length)? res.status(404).send(`Tea don't exist`):''
+  teaData[index] = { id:parseInt(req.params.id), tea:req.body.tea, price :req.body.price};
 
-  res.status(200).send(tea);
+  res.status(200).send({tea:teaData[index],response:`Tea with ID:${index + 1} is update`});
+
 });
 
-//delete tea
+//delete a tea
 
-app.delete("/teas/:id", (req, res) => {
-  const index = teaData.findIndex((tea) => tea.id != parseInt(req.params.id));
-  if (index == -1) {
-    return res.status(404).send("Tea not found");
-  }
-  teaData.splice(index, 1);
+app.delete('/teas/:id',(req,res)=>{
+  const index = parseInt(req.params.id) - 1
 
-  return res.status(200).send("Tea deleted");
-});
+  res.status(200).send(teaData.splice(index,1))
+})
 
 
-
-app.listen(port, () => {
-  console.log("Server is listening on port 3000");
+app.listen(PORT, () => {
+  console.log(`connected to the port : ${PORT}`);
 });
