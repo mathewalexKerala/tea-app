1c82b22b030814657505eaaf9f4d7ce91ee147b2
Mathew Alex <mathewalexmasai@gmail.com>
2024-12-12 12:13:29 +0530

    make changes in logger, index.js

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
 
-const app = express(); 
+const morganFormat = `:method :url :status :response-time ms`;
+const app = express();
 
-const PORT =process.env.PORT || 3000;
+// app.use(
+//   morgan(morganFormat, {
+//     stream: {
+//       write: (message) => {
+//         const logObject = {
+//           method: message.split(" ")[0],
+//           url: message.split(" ")[1],
+//           status: message.split(" ")[2],
+//           responseTime: message.split(" ")[3],
+//         };
+//         logger.info(JSON.stringify(logObject));
+//       },
+//     },
+//   })
+// )
+app.use(logger)
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -17,20 +36,22 @@ app.post("/teas", (req, res) => {
     tea,
     price,
   };
+  
   teaData.push(newTea);
-   res.status(201).send(newTea);
+  
+  res.status(201).send(newTea);
 });
 
 //get all tea
 app.get("/teas", (req, res) => {
-   res.status(200).send(teaData);
+  res.status(200).send(teaData);
 });
 
 //get single tea
 app.get("/teas/:id", (req, res) => {
   const Id = req.params.id - 1;
- 
-   Id < teaData.length && Id >= 0
+
+  Id < teaData.length && Id >= 0
     ? res.status(200).send(teaData[Id])
     : res.status(404).send("not found");
 });
@@ -38,25 +59,34 @@ app.get("/teas/:id", (req, res) => {
 //update an existing tea
 
 app.put("/teas/:id", (req, res) => {
- 
- 
   const index = parseInt(req.params.id) - 1;
-//logic for not entering body or params id
-  ( !req.body || Object.keys(req.body).length ===0)?res.status(400).send(' Input value  is missing'):(index < 0 || index >=teaData.length)? res.status(404).send(`Tea don't exist`):''
-  teaData[index] = { id:parseInt(req.params.id), tea:req.body.tea, price :req.body.price};
+  //logic for not entering body or params id
+  !req.body || Object.keys(req.body).length === 0
+    ? res.status(400).send(" Input value  is missing")
+    : index < 0 || index >= teaData.length
+    ? res.status(404).send(`Tea don't exist`)
+    : "";
+  teaData[index] = {
+    id: parseInt(req.params.id),
+    tea: req.body.tea,
+    price: req.body.price,
+  };
 
-  res.status(200).send({tea:teaData[index],response:`Tea with ID:${index + 1} is update`});
-
+  res
+    .status(200)
+    .send({
+      tea: teaData[index],
+      response: `Tea with ID:${index + 1} is update`,
+    });
 });
 
 //delete a tea
 
-app.delete('/teas/:id',(req,res)=>{
-  const index = parseInt(req.params.id) - 1
+app.delete("/teas/:id", (req, res) => {
+  const index = parseInt(req.params.id) - 1;
 
-  res.status(200).send(teaData.splice(index,1))
-})
-
+  res.status(200).send(teaData.splice(index, 1));
+});
 
 app.listen(PORT, () => {
   console.log(`connected to the port : ${PORT}`);
--- logger.js
+++ logger.js
@@ -1,26 +1,41 @@
-import { createLogger, format, transports } from "winston";
-const { combine, timestamp, json, colorize } = format;
+// import { createLogger, format, transports } from "winston";
+// const { combine, timestamp, json, colorize } = format;
+import fs from 'fs'
 
 // Custom format for console logging with colors
-const consoleLogFormat = format.combine(
-  format.colorize(),
-  format.printf(({ level, message, timestamp }) => {
-    return `${level}: ${message}`;
-  })
-);
+// const consoleLogFormat = format.combine(
+//   format.colorize(),
+//   format.printf(({ level, message, timestamp }) => {
+//     return `${level}: ${message}`;
+//   })
+// );
 
 // Create a Winston logger
-const logger = createLogger({
-  level: "info",
-  format: combine(colorize(), timestamp(), json()),
-  transports: [
-    new transports.Console({
-      format: consoleLogFormat,
-    }),
-    new transports.File({ filename: "app.log" }),
-  ],
-});
+// const logger = createLogger({
+//   level: "info",
+//   format: combine(colorize(), timestamp(), json()),
+//   transports: [
+//     new transports.Console({
+//       format: consoleLogFormat,
+//     }),
+//     new transports.File({ filename: "app.log" }),
+//   ],
+// });
 
+function logger(req,res,next){
+let start =new Date()
+  res.on('finish',()=>{
+start = new Date() - start
+
+    console.log(`Info :{"method":"${req.method}" ,"path":"${req.path}", "status":"${res.statusCode} "responseTime": "${start}"}`)
+    fs.appendFile('app.log',`Info :{"method":"${req.method}" ,"path":"${req.path}", "status":"${res.statusCode}, "responseTime": "${start}"}`,(err)=>{
+      if(err) {
+        console.log(`Error : ${err}`)
+      }
+    })
+  })
+  next()
+}
 export default logger;
 
 
